0819f5cff8f780675dff85b2bff45ac8168b00e9
karol-szykula <karol.szykula@wp.pl>
2019-01-24 14:47:16 +0100

    Add input to change link

--- src/App.js
+++ src/App.js
@@ -4,18 +4,39 @@ import {Header} from "./components/Header";
 import {Home} from "./components/Home";
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            homeLink: "Home"
+        };
+    }
+
+    onChangeLink = (newName) => {
+        this.setState({
+            homeLink: newName
+        })
+    };
+
+    onGreet() {
+        alert("Hello!");
+    };
 
     render() {
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Header/>
+                        <Header homeLink={this.state.homeLink}/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home name={"Max"} age={27}/>
+                        <Home
+                            onChangeLink={this.onChangeLink}
+                            initialLink={this.state.homeLink}
+                            name={"Max"}
+                            age={27} greet={this.onGreet}
+                        />
                     </div>
                 </div>
             </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,25 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 export class Home extends React.Component {
 
     state = {
-        age: this.props.age
+        age: this.props.age,
+        homeLink: "Link",
+        inputValue: ''
     };
 
 
     onMakeMeOlder = () => {
-        this.setState({age: this.state.age += 3})
+        let age = this.state.age + 3;
+        this.setState({age: age});
+    };
+
+    onChangeName = () => {
+        this.props.onChangeLink(this.state.inputValue);
+    };
+
+    onHandleChange = (event) => {
+        this.setState({inputValue: event.target.value})
     };
 
     render() {
@@ -20,12 +30,16 @@ export class Home extends React.Component {
                 <p>Your age: {this.state.age}</p>
                 <hr/>
                 <button onClick={this.onMakeMeOlder} className={"btn btn-primary"}>Make me older!</button>
+                <hr/>
+                <button onClick={this.props.greet} className={"btn btn-primary"}>On Greet</button>
+                <hr/>
+                <input
+                    type={"text"}
+                    value={this.state.inputValue}
+                    onChange={(event) => this.onHandleChange(event)}
+                />
+                <button onClick={this.onChangeName} className={"btn btn-primary"}>On Change Link</button>
             </div>
         );
     }
-}
-
-Home.PropTypes = {
-    name: PropTypes.string,
-    age: PropTypes.number,
 }
