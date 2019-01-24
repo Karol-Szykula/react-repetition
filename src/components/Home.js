acd33f42a2758050d988021855498f6bb72281ce
karol-szykula <karol.szykula@wp.pl>
2019-01-24 11:59:41 +0100

    Add state to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,31 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export class Home extends React.Component {
+
+    state = {
+        age: this.props.age
+    };
+
+
+    onMakeMeOlder = () => {
+        this.setState({age: this.state.age += 3})
+    };
+
     render() {
         return (
             <div>
                 <p>In a new Component!</p>
+                <p>Your name: {this.props.name}</p>
+                <p>Your age: {this.state.age}</p>
+                <hr/>
+                <button onClick={this.onMakeMeOlder} className={"btn btn-primary"}>Make me older!</button>
             </div>
         );
     }
+}
+
+Home.PropTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
 }
