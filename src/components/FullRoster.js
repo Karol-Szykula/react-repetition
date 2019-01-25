947896061976ef54761249308c04822606f71eea
karol-szykula <karol.szykula@wp.pl>
2019-01-25 14:07:53 +0100

    Prepare repo for new tutorial

--- src/components/FullRoster.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const FullRoster = (props) => (
-    <div>
-        FullRoster
-    </div>
-);
-
-export default FullRoster;
--- src/components/Schedule.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Schedule = (props) => (
-    <div>
-        Schedule
-    </div>
-);
-
-export default Schedule;
