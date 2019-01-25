87adaf0e29695d688768eb0d691341bb919b616a
karol-szykula <karol.szykula@wp.pl>
2019-01-25 09:09:09 +0100

    Prepare repository for react-router

--- src/App.js
+++ src/App.js
@@ -1,70 +1,17 @@
-import React from 'react';
+import React from "react";
+import {Link} from "react-router";
 
-import {Header} from "./components/Header";
-import {Home} from "./components/Home";
-
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            homeLink: "Home",
-            homeMounted: true
-        };
-    }
-
-    onChangeLink = (newName) => {
-        this.setState({
-            homeLink: newName
-        })
-    };
-
-    onGreet() {
-        alert("Hello!");
-    };
-
-    onChangeHomeMounted = () => {
-        this.setState({
-            homeMounted: !this.state.homeMounted
-        })
-    }
-
-    render() {
-
-        let homeComponent = '';
-        if (this.state.homeMounted) {
-            homeComponent = (
-                <Home
-                    onChangeLink={this.onChangeLink}
-                    initialLink={this.state.homeLink}
-                    name={"Max"}
-                    age={27} greet={this.onGreet}
-                />
-            );
-        }
-
-        return (
+export const Header = (props) => {
+    return (
+        <nav className="navbar navbar-default">
             <div className="container">
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                        <Header homeLink={this.state.homeLink}/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                        {homeComponent}
-                    </div>
-                </div>
-
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                        <button onClick={this.onChangeHomeMounted} className={"btn btn-primary"}>(Un)Mount home
-                            component
-                        </button>
-                    </div>
+                <div className="navbar-header">
+                    <ul className="nav navbar-nav">
+                        <li><Link to={"/home"} activeStyle={{color: "red"}}>Home</Link></li>
+                        <li><Link to={"/user/10"} activeClassName={"active"}>User</Link></li>
+                    </ul>
                 </div>
             </div>
-        );
-    }
-}
-
-export default App;
+        </nav>
+    );
+};
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,76 +1,10 @@
 import React from "react";
 
 export class Home extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            age: this.props.age,
-            homeLink: "Link",
-            inputValue: ''
-        };
-        console.log('constructor')
-    };
-
-
-    componentWillMount() {
-        console.log("Component will mount");
-    };
-
-    componentDidMount() {
-        console.log("did mount");
-    }
-
-    componentWillReceiveProps(nextProps, nextContext) {
-        console.log("Receive props: ", nextProps);
-    };
-
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        console.log("Should component update: ", nextProps, nextState);
-        return true;
-    };
-
-    componentWillUpdate(nextProps, nextState, nextContext) {
-        console.log("will update: ", nextProps, nextState);
-    };
-
-    componentDidUpdate(prevProps, prevState) {
-        console.log("did update: ", prevProps, prevState);
-    };
-
-    componentWillUnmount() {
-        console.log("component will unmount");
-    };
-
-    onMakeMeOlder = () => {
-        let age = this.state.age + 3;
-        this.setState({age: age});
-    };
-
-    onChangeName = () => {
-        this.props.onChangeLink(this.state.inputValue);
-    };
-
-    onHandleChange = (event) => {
-        this.setState({inputValue: event.target.value})
-    };
-
     render() {
         return (
             <div>
-                <p>In a new Component!</p>
-                <p>Your name: {this.props.name}</p>
-                <p>Your age: {this.state.age}</p>
-                <hr/>
-                <button onClick={this.onMakeMeOlder} className={"btn btn-primary"}>Make me older!</button>
-                <hr/>
-                <button onClick={this.props.greet} className={"btn btn-primary"}>On Greet</button>
-                <hr/>
-                <input
-                    type={"text"}
-                    value={this.state.inputValue}
-                    onChange={(event) => this.onHandleChange(event)}
-                />
-                <button onClick={this.onChangeName} className={"btn btn-primary"}>On Change Link</button>
+                <h3>Home</h3>
             </div>
         );
     }
