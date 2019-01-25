766a0cc7e96f3fef961eb81c523476a86b9c15cd
karol-szykula <karol.szykula@wp.pl>
2019-01-25 15:11:03 +0100

    Working router example

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import {Link, Route, Switch} from 'react-router-dom';
+
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+    </div>
+);
+
+const Category = () => (
+    <div>
+        <h2>Category</h2>
+    </div>
+);
+
+const Products = () => (
+    <div>Products</div>
+);
 
 class App extends React.Component {
 
     render() {
         return (
             <div>
-                App
+                <nav className={'navbar navbar-light'}>
+
+                    <ul className={'nav navbar-nav'}>
+
+                        <li><Link to={'/'}>Homes</Link></li>
+                        <li><Link to={'/category'}>Category</Link></li>
+                        <li><Link to={'/products'}>Products</Link></li>
+
+                    </ul>
+                </nav>
+
+                <Route path={'/'} component={Home}/>
+                <Route path={'/category'} component={Category}/>
+                <Route path={'/products'} component={Products}/>
+
             </div>
         )
     }
