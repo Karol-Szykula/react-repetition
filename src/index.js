3fc8ada93f819cbfb964ad283e068972e86266f6
karol-szykula <karol.szykula@wp.pl>
2019-01-28 11:19:21 +0100

    Fix reducer

--- src/index.js
+++ src/index.js
@@ -14,14 +14,18 @@ import {createStore} from "redux";
 const reducer = (state, action) => {
     switch (action.type) {
         case "ADD":
-            state = state + action.value;
-            break;
-        case "SUBSTRACT":
+            return {
+                ...state,
+                state: state + action.payload
+            };
+        case
+        "SUBSTRACT"
+        :
             break;
         default:
             return state;
     }
-}
+};
 
 const store = createStore(reducer, 1);
 
