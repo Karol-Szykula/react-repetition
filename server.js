27cde4e7fd6492df869976d1487cd17f1c03110e
karol-szykula <karol.szykula@wp.pl>
2019-01-28 15:08:58 +0100

    Add app module

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-const htttp = require('http');
+const http = require('http');
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(port);
